2e5703476e3e57457cb938cac693906d90a5b3e4
LAPTOP-JHLSCDA7\steve <steve@soaconsultingservices.com>
2020-09-22 19:28:08 -0400

    center text

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {ApplicationComponent} from './components/application/application';
+import NavigationComponent from './components/navigation/navigation';
+import FooterComponent from './components/footer/footer';
+import ProductListComponent from './components/productlist/productlist';
+import {ProductService} from './services/product.service';
 
 @NgModule({
+  imports: [BrowserModule],
   declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    ApplicationComponent,
+    FooterComponent,
+    ProductListComponent,
+    NavigationComponent],
+  providers: [ProductService],
+  bootstrap: [ApplicationComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
